1c18b6ec98833a7b305f9a9710f5825c4a2726e8
psped1 <psped1@hotmail.com>
2020-07-07 21:50:24 +1000

    Fixed problem in ex2.4. Note: 1-d arrays are row vectors by default, so declare column vectors as n*1 arrays

--- ch2/ex2.4.cpp
+++ ch2/ex2.4.cpp
@@ -6,44 +6,44 @@
 */
 #include<iostream>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[0])
 {
-	double u[3] = { 1.0, 2.0, 3.0};
-	double v[3] = {6.0, 5.0, 4.0};
-	double A[3][3] ={{1.0, 5.0, 4.0},
+	const double u[3][1] = { {1.0}, {2.0}, {3.0}};
+	const double v[3][1] = {{6.0}, {5.0}, {4.0}};
+	const double A[3][3] ={{1.0, 5.0, 0.0},
 					 {7.0, 1.0, 2.0},
 					 {0.0, 0.0, 1.0}};
-	double B[3][3] ={{-2.0, 0.0, 1.0},
+	const double B[3][3] ={{-2.0, 0.0, 1.0},
 					 { 1.0, 0.0, 0.0},
 					 { 4.0, 1.0, 0.0}};
-	double w[3];
+	double w[3][1];
 	for (int i=0; i<3; i++)
 	{
-		w[i] = u[i] - 3.0*v[i];
+		w[i][0] = u[i][0] - 3.0*v[i][0];
 	}
 
 	//Begin the exercise from here
 //x
-	double x[3];
+	double x[3][1];
 	for (int i=0; i<3; i++)
-	{	x[i] = u[i] - v[i];  }
+	{	x[i][0] = u[i][0] - v[i][0];  }
 //y
-	double y[3];
+	double y[3][1];
 	for (int i=0; i<3; i++)
 	{
-		y[i] = 0; //initialise the sum
+		y[i][0] = 0; //initialise the sum
 
 		for (int j=0; j<3; j++)
 		{
-			y[i] += (A[i][j] * u[j]);
+			y[i][0] += (A[i][j] * u[j][0]);
 		}
 	}
 
 //z
-	double z[3];
+	double z[3][1];
 	for (int i=0; i<3; i++)
 	{
-		z[i] = y[i] - v[i];
+		z[i][0] = y[i][0] - v[i][0];
 	}
 
 //C
@@ -58,18 +58,20 @@ int main(int argc, char* argv[])
 
 //D
 	double D[3][3];
-	for(int i=0; i<3; i++)
+	double accumulator = 0.0;
+
+	for(int dRow = 0; dRow < 3; dRow++)
 	{
-		for(int j=0; j<3; j++)
+		for(int dCol=0; dCol<3; dCol++)
 		{
-		//I think i did something wrong
+			for(int i=0; i<3; i++)
+			{
+				accumulator += A[dRow][i] * B[i][dCol];
+			}
+			D[dRow][dCol] = accumulator;
+			accumulator = 0;
 		}
 	}
-
-
-
-
-
 
 	//Print output
 	std::cout << "Printing the variables:\n";
@@ -77,12 +79,12 @@ int main(int argc, char* argv[])
 	//u
 	std::cout << "\nu = \n";
 	for(int i = 0; i < 3; i++)
-	{std::cout << u[i] << "\t";}
+	{std::cout << u[i][0] << "\t";}
 
 	//v
 	std::cout << "\nv = \n";
 	for(int i = 0; i < 3; i++)
-	{std::cout << v[i] << "\t";}
+	{std::cout << v[i][0] << "\t";}
 
 	//A
 	std::cout << "\nA = \n";
@@ -105,21 +107,44 @@ int main(int argc, char* argv[])
 		}
 		std::cout << "\n";
 	}
+	
+	std::cout << "\nHere are the calculated results:\n";
 
 	//w
 	std::cout << "\n\nw = \n";
 	for(int i = 0; i < 3; i++)
-	{std::cout << w[i] << "\t";}
+	{std::cout << w[i][0] << "\t";}
 
 	//x
 	std::cout << "\n\nx = \n";
 	for(int i = 0; i < 3; i++)
-	{std::cout << x[i] << "\t";}
+	{std::cout << x[i][0] << "\t";}
 
 	//y
 	std::cout << "\n\ny = \n";
 	for(int i = 0; i < 3; i++)
-	{std::cout << y[i] << "\t";}
+	{std::cout << y[i][0] << "\t";}
 
+	//C
+	std::cout << "\nC = \n";
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			std::cout << C[i][j] << "\t";
+		}
+		std::cout << "\n";
+	}
+
+	//D
+	std::cout << "\nD = \n";
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			std::cout << D[i][j] << "\t";
+		}
+		std::cout << "\n";
+	}
 	return 0;
 }
